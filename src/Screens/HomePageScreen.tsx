36441fee71add27b1f65f770583f0d33b4950ea5
Praveen-Dixit07 <dixitpraveen03072002@gmail.com>
2024-06-03 12:31:57 +0530

    initial testing by praveen

--- src/Screens/HomePageScreen.tsx
+++ src/Screens/HomePageScreen.tsx
@@ -1,10 +1,10 @@
-import {StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
 const HomePageScreen = () => {
   return (
     <View>
-      <Text>HomePageScreen</Text>
+      <Text>hiii its praveen</Text>
     </View>
   );
 };
