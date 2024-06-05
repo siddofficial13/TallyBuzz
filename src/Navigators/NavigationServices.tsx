d8708cfbd16e8b0a06edabff88f5a302671dd8a8
Praveen-Dixit07 <dixitpraveen03072002@gmail.com>
2024-06-05 15:54:31 +0530

    updated uploadpost page and homepage screen

---
+++ src/Navigators/NavigationServices.tsx
@@ -0,0 +1,29 @@
+import { NavigationContainerRef, CommonActions } from '@react-navigation/native';
+
+let _navigator: NavigationContainerRef<any> | null = null;
+
+function setTopLevelNavigator(navigatorRef: NavigationContainerRef<any> | null) {
+    _navigator = navigatorRef;
+}
+
+function navigate(routeName: string, params?: object) {
+    if (_navigator && _navigator.isReady()) {
+        _navigator.navigate(routeName, params);
+    } else {
+        console.warn("Navigation container is not ready or navigator reference is null.");
+    }
+}
+
+function goBack() {
+    if (_navigator && _navigator.isReady()) {
+        _navigator.dispatch(CommonActions.goBack());
+    } else {
+        console.warn("Navigation container is not ready or navigator reference is null.");
+    }
+}
+
+export default {
+    navigate,
+    setTopLevelNavigator,
+    goBack,
+};
